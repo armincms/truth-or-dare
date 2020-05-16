2b5b4c6986b78382bb4149dd02c2b60cce20886c
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-05-16 10:18:30 +0430

    Fixes player owner relation ship

--- src/Player.php
+++ src/Player.php
@@ -1,9 +1,14 @@
 <?php
 
-namespace Armincms\TruthOrDare; 
+namespace Armincms\TruthOrDare;
 
-class Player extends Model 
+use Armincms\Contracts\Authorizable;
+use Armincms\Concerns\Authorization; 
+
+class Player extends Model implements Authorizable
 {  
+	use Authorization;
+	
 	public function games()
 	{
 		return $this->belongsToMany(Game::class, 'tod_game_player');
